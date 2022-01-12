aada3d71fd6328f6064c381b48095f650f16a062
mateusmachado3594@gmail.com <mateusmachado3594@gmail.com>
2022-01-11 22:00:51 -0300

    OK

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Somador</title>
+</head>
+<body>
+   <form action='PHP.Somador' method='post'>
+        <input num1="Num1" type="number" placeholder= "Numero1">
+        <input num2="Num2" type="number" placeholder= "Numero2">
+        <button type = 'submit'>Soma</button>
+    <form>
+</body>
+</html>
+
+
